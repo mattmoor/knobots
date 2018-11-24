30d53ffd50ab0ddf1357bca8deb8ddb521a2a789
Matt Moore <mattmoor@google.com>
2018-11-24 15:56:19 +0000

    Restructure the repository to prepare for multiple bots to coexist.
    Tweak the "hello" bot to be a "debug" bot that we can use to debug github webhook behaviors.

---
+++ cmd/debug/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("ERROR: no payload: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// TODO(mattmoor): This should be:
+	//     eventType := github.WebHookType(r)
+	// https://github.com/knative/eventing-sources/issues/120
+	// HACK HACK HACK
+	eventType := strings.Split(r.Header.Get("ce-eventtype"), ".")[4]
+
+	event, err := github.ParseWebHook(eventType, payload)
+	if err != nil {
+		log.Printf("ERROR: unable to parse webhook: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// The set of events here should line up with what is in
+	//   config/one-time/github-source.yaml
+	switch event := event.(type) {
+	case *github.PullRequestEvent:
+		if err := HandlePullRequest(event); err != nil {
+			log.Printf("Error handling %T: %v", event, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	case *github.IssuesEvent:
+		if err := HandleIssues(event); err != nil {
+			log.Printf("Error handling %T: %v", event, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	default:
+		log.Printf("Unrecognized event: %T", event)
+		http.Error(w, "Unknown event", http.StatusBadRequest)
+		return
+	}
+}
+
+func HandleIssues(ie *github.IssuesEvent) error {
+	log.Printf("Issue: %v", ie.GetIssue().String())
+
+	ctx := context.Background()
+	ghc := GetClient(ctx)
+
+	msg := fmt.Sprintf("Issues event: %v", ie.GetAction())
+	_, _, err := ghc.Issues.CreateComment(ctx,
+		ie.Repo.Owner.GetLogin(), ie.Repo.GetName(), ie.GetIssue().GetNumber(),
+		&github.IssueComment{
+			Body: &msg,
+		})
+	return err
+}
+
+func HandlePullRequest(pre *github.PullRequestEvent) error {
+	log.Printf("PR: %v", pre.GetPullRequest().String())
+
+	ctx := context.Background()
+	ghc := GetClient(ctx)
+
+	msg := fmt.Sprintf("PR event: %v", pre.GetAction())
+	_, _, err := ghc.Issues.CreateComment(ctx,
+		pre.Repo.Owner.GetLogin(), pre.Repo.GetName(), pre.GetNumber(),
+		&github.IssueComment{
+			Body: &msg,
+		})
+	return err
+}
+
+func GetClient(ctx context.Context) *github.Client {
+	return github.NewClient(
+		oauth2.NewClient(ctx,
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN"),
+				},
+			),
+		),
+	)
+}
