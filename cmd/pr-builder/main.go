1cec8b8637757e1ff7ce03f3ba085e805334be47
Matt Moore <mattmoor@vmware.com>
2019-11-12 12:23:07 -0800

    Change assignees, delete prettier for docs.

--- cmd/pr-builder/main.go
+++ cmd/pr-builder/main.go
@@ -31,7 +31,7 @@ var (
 	// TODO(mattmoor): Figure out how to dodge CLA bot...
 	signature = &object.Signature{
 		Name:  "Matt Moore (via sockpuppet)",
-		Email: "mattmoor+sockpuppet@vmware.com",
+		Email: "mattmoor@vmware.com",
 		When:  time.Now(),
 	}
 
